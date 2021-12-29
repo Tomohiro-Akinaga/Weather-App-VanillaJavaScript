718caed409bfc799054c351367f97c25796eadc6
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2021-12-28 17:43:34 -0800

    修正

--- Autocomplete.js
+++ Autocomplete.js
@@ -1,4 +1,4 @@
-// Usage of API in https://countrystatecity.in/docs/api/all-countries/
+// Call API in https://countrystatecity.in/docs/api/all-countries/
 const headers = new Headers();
 headers.append("X-CSCAPI-KEY", "T2JNanhMaDNoRkp4eU5HbEZ4T2pVZHpQQktVZ0NHN216N2V5aHBWYQ==");
 
@@ -8,78 +8,74 @@ const requestOptions = {
     refirect: "follow"
 };
 
-// Fetch the country data
-const array = [];
-const suggestArray = [];
-
-fetch("https://api.countrystatecity.in/v1/countries", requestOptions)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        return countryData(data);
-    })
-    .catch(error => {
-        return console.log(error);
-    });
-
-function countryData(jsonData) {
-
-    for ( let i = 0; i < jsonData.length; i++ ) {
-        array.push(jsonData[i].name)
-    }  
-
+async function callapi() {
+    const res = await fetch("https://api.countrystatecity.in/v1/countries", requestOptions);
+    const data = await res.json();
+    console.log(data);
 };
+callapi();
+
+
+
+
+
+
+
+
+// function countryData(jsonData) {
+
+//     for ( let i = 0; i < jsonData.length; i++ ) {
+//         array.push(jsonData[i].name)
+//     }  
+
+// };
 
 // Fetch the state data
-fetch("https://api.countrystatecity.in/v1/states", requestOptions)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        return stateData(data);
-    })
-    .catch(error => {
-        return console.log(error);
-    });
+// fetch("https://api.countrystatecity.in/v1/states", requestOptions)
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(data => {
+//         return stateData(data);
+//     })
+//     .catch(error => {
+//         return console.log(error);
+//     });
 
-function stateData(jsonData) {
+// function stateData(jsonData) {
 
-    for ( let i = 0; i < jsonData.length; i++ ) {
-        array.push(jsonData[i].name)
-    } 
+//     for ( let i = 0; i < jsonData.length; i++ ) {
+//         array.push(jsonData[i].name)
+//     } 
     
-};
-
+// };
+// console.log(array);
 // Autocomplete function from below
-window.onload = function () {
-    const input = document.getElementById("input");
-    input.addEventListener("input", autocomplete);
-};
+// window.onload = function () {
+//     const input = document.getElementById("input");
+//     input.addEventListener("input", autocomplete);
+// };
 
-function autocomplete() {
 
-    const datalist = document.getElementById("name-list");
-    datalist.innerHTML = "";
+// function autocomplete() {
+    
+//     const suggestArray = [];
+//     const datalist = document.getElementById("name-list");
+//     datalist.innerHTML = "";
 
-    suggestArray.length = 0;
-    array.forEach(element => {
-        if ( element.includes(' ') ) {
-            const awesome = element.split(' ');
-            
-            for (let i = 0; i < awesome.length; i++ ) {
-                awesome[i].charAt(0).toLowerCase();
-                
-            }  
-            
-        }
-            
-    });
-};
 
-    // for ( let i = 0; i < 9; i ++ ) {
-    //     const datalist = document.getElementById("name-list");
-    //     const option = document.createElement("option");
-    //     option.value = suggestArray[i];
-    //     datalist.appendChild(option);
-    // }
+//     array.forEach(element => {
+
+//         // In case of not including space
+//         if ( element.includes(' ') === false ) {
+//             const str1 = element.charAt(0).toLowerCase() + element.slice(1);
+//             if ( str1.startsWith(input.value) && input.value.length > 0 ) {
+//                 console.log(element);
+//             }
+//         } else {
+//             ;
+//         }
+        
+//     });
+   
+// };
