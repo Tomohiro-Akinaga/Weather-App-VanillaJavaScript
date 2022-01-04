3bfda5a9b00a816533a58b05ad5fafbed914af66
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-01-04 15:59:05 -0800

    finished loading function

--- Loading.js
+++ Loading.js
@@ -1,8 +1,6 @@
 "use strict";
 
 export function finishLoading() {
-    const spinner = document.getElementById('loading');
-    spinner.classList.add('loaded');
-    const container = document.getElementById("container");
-    container.classList.add("container");
+    const loading = document.getElementById('loading');
+    loading.classList.add('loaded');
 };
