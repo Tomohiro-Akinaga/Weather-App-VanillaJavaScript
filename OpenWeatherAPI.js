0c0029c54ba133b4a5afc5019d91a557f277b920
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-01-05 19:41:23 -0800

    --allow-empty-message

--- OpenWeatherAPI.js
+++ OpenWeatherAPI.js
@@ -1,10 +1,29 @@
 "use strict";
 import { finishLoading } from "./Loading.js";
 
-const text = document.getElementById("input");
-const form = document.getElementById("form");
-form.addEventListener("submit", () => {
-    if ( text.value.length === 0 ) {
-        alert("Use country name or state name");
-    }
-});
+// When clicked form
+export function submitForm () {
+
+    
+    const form = document.getElementById("form");
+
+    form.addEventListener("submit", (event) => {
+        if ( text.value.length === 0 ) {
+            event.preventDefault();
+            alert("Use country name or state name");
+        } else {
+            event.preventDefault();
+            alert(cityName);
+        }
+    });
+
+};
+// Call the current weather API
+
+
+// const currentWeatherUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName +"&appid=7d20d69e5d5abc8385c9ae6416019816";
+
+// async function currentWeatherApi() {
+//     const currentWeatherData = await fetch(currentWeatherUrl).then(res => res.json());
+//     console.log(currentWeatherData);
+// };
