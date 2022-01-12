e63c8f8e4523a9c48b4ad2b4753e42d0b84d4768
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-01-11 20:22:27 -0800

    finished daily part

--- OpenWeatherAPI.js
+++ OpenWeatherAPI.js
@@ -69,6 +69,7 @@ function applyWeatherForecastHourly(jsonDataHourly) {
     const imgHourlyPlus3 = document.getElementById("container--img__hourly__plus3");
     const imgHourlyPlus4 = document.getElementById("container--img__hourly__plus4");
     const imgHourlyPlus5 = document.getElementById("container--img__hourly__plus5");
+    
     imgHourlyPlus0.src = `http://openweathermap.org/img/wn/${jsonDataHourly.hourly[0].weather[0].icon}@2x.png`;
     imgHourlyPlus1.src = `http://openweathermap.org/img/wn/${jsonDataHourly.hourly[1].weather[0].icon}@2x.png`;
     imgHourlyPlus2.src = `http://openweathermap.org/img/wn/${jsonDataHourly.hourly[2].weather[0].icon}@2x.png`;
@@ -82,6 +83,7 @@ function applyWeatherForecastHourly(jsonDataHourly) {
     const temperatureHourly3 = document.getElementById("container--temperature__hourly__3");
     const temperatureHourly4 = document.getElementById("container--temperature__hourly__4");
     const temperatureHourly5 = document.getElementById("container--temperature__hourly__5");
+    
     temperatureHourly0.innerText = Math.round(jsonDataHourly.hourly[0].temp) + "°";
     temperatureHourly1.innerText = Math.round(jsonDataHourly.hourly[1].temp) + "°";
     temperatureHourly2.innerText = Math.round(jsonDataHourly.hourly[2].temp) + "°";
@@ -89,25 +91,49 @@ function applyWeatherForecastHourly(jsonDataHourly) {
     temperatureHourly4.innerText = Math.round(jsonDataHourly.hourly[4].temp) + "°";
     temperatureHourly5.innerText = Math.round(jsonDataHourly.hourly[5].temp) + "°";
     
-    // undisplay();
+    undisplay();
 };
 
 /* undisplay daily and desc */
-// function undisplay() {
-//     const navItemDaily = document.getElementById("container--box__daily");
-//     const navItemDesc = document.getElementById("container--box__desc");    
-//     navItemDaily.style.display = "none";
-//     navItemDesc.style.display = "none";
-// };
+function undisplay() {
+    const navItemDaily = document.getElementById("container--box__daily");
+    // const navItemDesc = document.getElementById("container--box__desc");    
+    navItemDaily.style.display = "none";
+    // navItemDesc.style.display = "none";
+};
 
 /* Apply the forecast daily data to HTML */
-function applyWeatherForecastDaily() {
+function applyWeatherForecastDaily(jsonDataDaily) {
 
     displayDay();
-    const navItemDaily = document.getElementById("container--nav-item__daily"); 
+    console.log(jsonDataDaily);
+    const imgDailyPlus1 = document.getElementById("container--img__daily__plus1");
+    const imgDailyPlus2 = document.getElementById("container--img__daily__plus2");
+    const imgDailyPlus3 = document.getElementById("container--img__daily__plus3");
+    const imgDailyPlus4 = document.getElementById("container--img__daily__plus4");
+    const imgDailyPlus5 = document.getElementById("container--img__daily__plus5");
+    const imgDailyPlus6 = document.getElementById("container--img__daily__plus6");
 
+    imgDailyPlus1.src = `http://openweathermap.org/img/wn/${jsonDataDaily.daily[1].weather[0].icon}@2x.png`;
+    imgDailyPlus2.src = `http://openweathermap.org/img/wn/${jsonDataDaily.daily[2].weather[0].icon}@2x.png`;
+    imgDailyPlus3.src = `http://openweathermap.org/img/wn/${jsonDataDaily.daily[3].weather[0].icon}@2x.png`;
+    imgDailyPlus4.src = `http://openweathermap.org/img/wn/${jsonDataDaily.daily[4].weather[0].icon}@2x.png`;
+    imgDailyPlus5.src = `http://openweathermap.org/img/wn/${jsonDataDaily.daily[5].weather[0].icon}@2x.png`;
+    imgDailyPlus6.src = `http://openweathermap.org/img/wn/${jsonDataDaily.daily[6].weather[0].icon}@2x.png`;
     
-
+    const temperatureDaily1 = document.getElementById("container--temperature__daily__1");
+    const temperatureDaily2 = document.getElementById("container--temperature__daily__2");
+    const temperatureDaily3 = document.getElementById("container--temperature__daily__3");
+    const temperatureDaily4 = document.getElementById("container--temperature__daily__4");
+    const temperatureDaily5 = document.getElementById("container--temperature__daily__5");
+    const temperatureDaily6 = document.getElementById("container--temperature__daily__6");
+    
+    temperatureDaily1.innerText = Math.round(jsonDataDaily.daily[1].temp.day) + "°";
+    temperatureDaily2.innerText = Math.round(jsonDataDaily.daily[2].temp.day) + "°";
+    temperatureDaily3.innerText = Math.round(jsonDataDaily.daily[3].temp.day) + "°";
+    temperatureDaily4.innerText = Math.round(jsonDataDaily.daily[4].temp.day) + "°";
+    temperatureDaily5.innerText = Math.round(jsonDataDaily.daily[5].temp.day) + "°";
+    temperatureDaily6.innerText = Math.round(jsonDataDaily.daily[6].temp.day) + "°";
 };
 
 /* Execute the function for Canada keyword as default*/
